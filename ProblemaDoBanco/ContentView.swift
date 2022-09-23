091fb077c18c1c14e83d624ac5ad2dd33adb02ba
joaoipiraja <joaovictorpiraja@gmail.com>
2022-09-23 12:05:52 -0300

    Initial Commit

---
+++ ProblemaDoBanco/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ProblemaDoBanco
+//
+//  Created by João Victor Ipirajá de Alencar on 23/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProblemaDoBanco/ProblemaDoBancoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProblemaDoBancoApp.swift
+//  ProblemaDoBanco
+//
+//  Created by João Victor Ipirajá de Alencar on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ProblemaDoBancoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
